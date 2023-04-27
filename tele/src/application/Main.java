c424efc525da57119aee94df39c12f62142eaeae
NickyRi <131983519+NickyRi@users.noreply.github.com>
2023-04-27 18:16:39 +0200

    title

--- tele/src/application/Main.java
+++ tele/src/application/Main.java
@@ -16,7 +16,7 @@ public class Main extends Application {
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
-			primaryStage.setTitle("Napoli campione");
+			primaryStage.setTitle("Napoli");
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
