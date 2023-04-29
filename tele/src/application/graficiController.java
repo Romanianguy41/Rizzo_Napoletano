aa3e1d3c897ac9c653494f9e72c84f0f03264a86
NickyRi <131983519+NickyRi@users.noreply.github.com>
2023-04-29 17:36:03 +0200

    import non utilizzati

--- tele/src/application/graficiController.java
+++ tele/src/application/graficiController.java
@@ -4,10 +4,7 @@ import java.io.IOException;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.chart.LineChart;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
