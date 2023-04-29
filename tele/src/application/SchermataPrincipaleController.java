22e673f5d1693684f3fac04c23ebb64340708b1b
NickyRi <131983519+NickyRi@users.noreply.github.com>
2023-04-29 17:43:49 +0200

    fozza napoli

--- tele/src/application/SchermataPrincipaleController.java
+++ tele/src/application/SchermataPrincipaleController.java
@@ -10,7 +10,6 @@ import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.effect.Effect;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
