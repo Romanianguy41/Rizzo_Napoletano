c054eb40ea375055406506897b9e62dc8a431dc1
NickyRi <131983519+NickyRi@users.noreply.github.com>
2023-05-23 12:00:58 +0200

    Spero funzioni sta scala logaritmica

--- tele/src/application/graficiController.java
+++ tele/src/application/graficiController.java
@@ -1,12 +1,11 @@
 package application;
 
 import java.io.IOException;
-import java.util.ArrayList;
-
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Node;
 import javafx.scene.chart.LineChart;
+import javafx.scene.chart.NumberAxis;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
 
@@ -17,11 +16,15 @@ public class graficiController {
     @FXML
     private Button back;
 
-    @FXML
-    private LineChart<?, ?> graficoFase;
+    final NumberAxis asseX = new NumberAxis(Math.log10(1), Math.log10(100), 1);
+	final NumberAxis asseY = new NumberAxis();
 
     @FXML
-    private LineChart<?, ?> graficoModulo;
+    private LineChart<?, ?> graficoFase = new LineChart<>(asseX, asseY);
+
+    @FXML
+    private LineChart<?, ?> graficoModulo = new LineChart<>(asseX, asseY);;
+
 
     @FXML
     void back(ActionEvent event) throws IOException 
